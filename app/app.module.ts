1ea1ad51c15d4fcc101c7b851abd87d60ee76acc
Zak Reynolds <zakary.reynolds@gmail.com>
2017-01-15 21:42:13 -0600

    Refactored Heroes into a component separate from app component.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,8 +2,9 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroesComponent } from './heroes.component';
 import { AppComponent }  from './app.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [
@@ -12,8 +13,10 @@ import { AppComponent }  from './app.component';
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [ HeroService ]
 })
 export class AppModule { }
